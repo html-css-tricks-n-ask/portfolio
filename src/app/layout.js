443415f36dd8b806d4cf230785af045296346743
bharat <bharat@bharats-MacBook-Air.local>
2026-01-14 02:48:43 +0530

    adding changes for Next version

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />{" "}
+        <meta name="theme-color" content="#000000" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
